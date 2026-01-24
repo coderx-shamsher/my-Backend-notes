ba179c01c89785cbe7da6c5a0f673b2531c3a5e5
coderx-shamsher <shamsher000789@gmail.com>
2026-01-24 09:19:03 +0530

    day4 backend tasks and topics

---
+++ backend-day4/day4.js
@@ -0,0 +1,28 @@
+// Today express.js Framework 
+// MERN Stack -- M -- Mongodb E-- Express.js R -- Reactjs N -- Node.js
+// or we will learn -- MEN -- 75 %  and the 25% -- > reactjs
+
+// Introductio to express.js 
+// Express.js ek npm package hai 
+// or yeah ek framework hai 
+// framework ka ek flow hota hai jis flow mein jo kam ho raha hai vo kam app kaise bhi kro but vo kam hona chaahie or follow the flow 
+// means jise bola hai vese he likhna parde ga us flow ko follow krna pardega.. 
+// ager bola hai k jb bhi koi module ko use kna hai to import kro to krna he pardega 
+// express js manages everthinks from receiving the request and giving the response 
+// NOTE ---- check the offical web to check the docs of expressjs 
+
+
+// Setting up a basic Express application 
+// and we also handle the requests  and responses a little bit 
+// go into expressjs 
+
+
+
+// Middleware 
+// ager requests k server routes tak janne say pahle ager ham us request par kuch or operation krna chachte hein k mean us request ko routes say pahle process krna hai exmaple 
+// mai chaahta hun k request k server tak aate he hello print ho jaye then routing ho to use hora hai middleware just ek function jo requests ko manipulate krta hai before the routes handle 
+// also check the md file 
+
+
+// error handing in expressjs 
+// just go on the express js website to check how to handle errors in express 
