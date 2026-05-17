ddd538567eb5be3803719e82aa62c2b811779661
coderx-shamsher <shamsher000789@gmail.com>
2026-05-17 08:45:46 +0200

    creating products model with mongoose

---
+++ backend_Projects/backend_project_1/eternity/models/product_schema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+// mongoose.connect("mongodb://localhost:27017/eternity")
+
+const ProductSchema= mongoose.Schema({
+    
+    ProductName: String,
+    Image : String,
+    Price: Number,
+    discount: {
+        type:Number,
+        default: 0
+    },
+    bgcolor: String,
+    panlecolor: String,
+    Textcolor : String
+})
+ 
+module.exports = mongoose.model("products",ProductSchema)
