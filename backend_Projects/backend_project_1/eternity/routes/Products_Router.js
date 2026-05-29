63d4ee07b89c84b39027c708c2e6482e9b11dd7c
coderx-shamsher <shamsher000789@gmail.com>
2026-05-29 19:44:16 +0200

    setup routers in router folder .....

---
+++ backend_Projects/backend_project_1/eternity/routes/Products_Router.js
@@ -0,0 +1,12 @@
+express = require("express")
+
+// create router using express.Router()
+const router = express.Router()
+
+// api setup with router or check kro k res mil raha hai k nhi... 
+router.get("/", (req,res)=>{
+    res.send("helloo admin users......")
+})
+
+
+module.exports = router
