a6fad2d3d2bd50663b2c664e8b86e6540a1cdf4b
coderx-shamsher <shamsher000789@gmail.com>
2026-05-31 14:19:21 +0200

    setup router and routes

--- backend_Projects/backend_project_1/eternity/src/app.js
+++ backend_Projects/backend_project_1/eternity/src/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3040
+
+// router require 
+const user_router = require("../routes/Users_Router")
+const Admin_router = require("../routes/Admins_Router")
+const  product_router = require("../routes/Products_Router")
 
 // 
 const cookie_parser = require("cookie-parser")
@@ -16,6 +21,16 @@ app.use(express.urlencoded({extended:true}))
 // static pages / public folder configuration 
 app.use(express.static(path.join(__dirname,"public")))
 
+// setup router using use() function
+
+// users route
+app.use("/users",user_router)
+
+// admins route
+app.use("/admins",Admin_router)
+
+// products route
+app.use("/products",product_router)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
