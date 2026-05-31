63ddbb9ef2d3f2feead1c23d12bbdef93894b4ae
coderx-shamsher <shamsher000789@gmail.com>
2026-05-31 14:22:40 +0200

    update router code ( 31-5-26)

--- backend_Projects/backend_project_1/eternity/routes/Products_Router.js
+++ backend_Projects/backend_project_1/eternity/routes/Products_Router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 // api setup with router or check kro k res mil raha hai k nhi... 
 router.get("/", (req,res)=>{
-    res.send("helloo admin users......")
+    res.send("helloo products......")
 })
 
 
